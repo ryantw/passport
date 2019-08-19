d2f519e68d4e4826423615ef94ba8c82984feabb
ryan <ryantwalk@gmail.com>
2019-08-19 14:31:47 -0400

    still implementing roles. #4, #7. Closes #6.

---
+++ src/test/java/io/lker/passport/service/RoleServiceImplTest.java
@@ -0,0 +1,37 @@
+package io.lker.passport.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
